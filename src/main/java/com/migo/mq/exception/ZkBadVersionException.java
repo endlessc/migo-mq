da8a8315050bb5e01b357672ec733ab744921b33
muyinchen <fei6751803@163.com>
2017-02-23 22:41:41 +0800

    对zookeeper做必要的工具类

---
+++ src/main/java/com/migo/mq/exception/ZkBadVersionException.java
@@ -0,0 +1,26 @@
+package com.migo.mq.exception;
+
+import org.apache.zookeeper.KeeperException;
+
+
+public class ZkBadVersionException extends ZkException {
+
+    private static final long serialVersionUID = 1L;
+
+    public ZkBadVersionException() {
+        super();
+    }
+
+    public ZkBadVersionException(KeeperException cause) {
+        super(cause);
+    }
+
+    public ZkBadVersionException(String message, KeeperException cause) {
+        super(message, cause);
+    }
+
+    public ZkBadVersionException(String message) {
+        super(message);
+    }
+}
+
